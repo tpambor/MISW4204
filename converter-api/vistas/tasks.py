169ef98b0a44a4f46ba8534a09d6af20c974aa0c
CamiloRamirezR <c.ramirezr2@uniandes.edu.co>
2023-10-17 23:27:02 -0500

    Endpoint terminado para "eliminar tarea"

--- converter-api/vistas/tasks.py
+++ converter-api/vistas/tasks.py
@@ -3,14 +3,14 @@ import datetime
 import pathlib
 from flask import current_app, send_from_directory, jsonify, url_for, request
 from flask.views import MethodView
-from flask_smorest import Blueprint
+from flask_smorest import Blueprint, abort
 import marshmallow as ma
 from flask_jwt_extended import jwt_required
 from db import db
 from modelos import Task, TaskStatus
 
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from flask import  request
+from flask import request
 
 blp = Blueprint("Tasks", __name__, description="API para gestionar tareas de conversión de formato")
 
@@ -33,11 +33,25 @@ TaskIdExample = [
         'newFormat': 'mp4'
     }
 ]
+TaskNotFoundExample = {
+    "code": 404,
+    "message": "Tarea no encontrada",
+    "status": "NOT_FOUND"
+}
+TasKUnprocessedExample = {
+    "code": 422,
+    "message": "La conversión no ha sido procesada",
+    "status": "UNPROCESSED"
+}
+
+
 class TaskInputFilesSchema(ma.Schema):
     fileName = ma.fields.Raw(required=True)
 
+
 class TaskInputFormSchema(ma.Schema):
     newFormat = ma.fields.String(required=True)
+
 
 class TaskSchema(ma.Schema):
     id = ma.fields.Integer()
@@ -53,6 +67,7 @@ class TaskSchema(ma.Schema):
         new_data['status'] = TaskStatus(new_data['status'])
         return new_data
 
+
 @blp.route("/api/tasks")
 class VistaTasks(MethodView):
     @blp.arguments(TaskInputFilesSchema, location="files")
@@ -66,11 +81,11 @@ class VistaTasks(MethodView):
         new_format = form['newFormat'].lower()
 
         new_task = Task(
-            created = datetime.datetime.utcnow(),
-            fileName = filename,
-            status = TaskStatus.UPLOADED,
-            oldFormat = old_format,
-            newFormat = new_format,
+            created=datetime.datetime.utcnow(),
+            fileName=filename,
+            status=TaskStatus.UPLOADED,
+            oldFormat=old_format,
+            newFormat=new_format,
         )
         db.session.add(new_task)
         db.session.commit()
@@ -89,6 +104,11 @@ class VistaTasks(MethodView):
     @blp.response(200, TaskSchema(many=True), description="Lista de tareas", example=TaskIdExample)
     @jwt_required()
     def get(self):
+        """
+        Lista de tareas
+        max: Cantidad máxima de tareas a obtener
+        order: Orden de las tareas (0: ascendente, 1: descendente)
+        """
         # obtener parametro 'max' de la url
         max_param = request.args.get('max', type=int)
         # obtener parametro 'order' de la url
@@ -107,17 +127,18 @@ class VistaTasks(MethodView):
             tasks = tasks.limit(max_param)
         # Obtener todas las tareas despues de filtrar y ordenar
         tasks = tasks.all()
-        # task = Task.query.all()
-        # return task
         return tasks
+
+
 @blp.route("/api/video/<path:filename>")
 class VistaVideo(MethodView):
-    def get(self,filename):
+    def get(self, filename):
         video_dir = current_app.config['VIDEO_DIR']
         original_video_path = os.path.join(video_dir, filename)
 
         if os.path.exists(original_video_path):
             return send_from_directory(video_dir, filename)
+
 
 @blp.route("/api/videos")
 class VistaVideos(MethodView):
@@ -126,39 +147,66 @@ class VistaVideos(MethodView):
 
         for video_path in ['1.mp4', '1.webm']:
             video_links[video_path] = url_for('Tasks.VistaVideo', filename=video_path).replace("/api", "")
-        
+
         return jsonify(video_links), 200
-    
+
+
 @blp.route("/api/tasks/<int:id_task>")
 class VistaTaskId(MethodView):
-    #@jwt_required
+    # @jwt_required
     @blp.doc(parameters=[{'name': 'id_task', 'in': 'path', 'description': 'ID de la tarea', 'required': True}])
-    @blp.response(200, TaskSchema(), description="Detalle de la tarea para poder obtener los links de descargar", example=TaskIdExample[0])
+    @blp.response(200, TaskSchema(), description="Detalle de la tarea para poder obtener los links de descargar",
+                  example=TaskIdExample[0])
     def get(self, id_task):
         """Detalle de una tarea
         
         Consultar el detalle de una tarea y para poder obtener los links de descargar
         """
-        #user_id = get_jwt_identity()
-        
+        # user_id = get_jwt_identity()
+
         task = Task.query.filter_by(id=id_task).first()
-        
+
         if not task:
             return {"message": "El archivo no fue encontrado"}, 404
 
         # Crea las URLs de descarga de archivos
-        
-        #video_dir = current_app.config['VIDEO_DIR']
-        
+
+        # video_dir = current_app.config['VIDEO_DIR']
+
         #  URLs de descarga
         original_video_url = url_for('Tasks.VistaVideo', filename=f'{task.id}.{task.oldFormat}').replace("/api", "")
         new_video_url = url_for('Tasks.VistaVideo', filename=f'{task.id}.{task.newFormat}').replace("/api", "")
 
-      # Diccionario con las URLs
+        # Diccionario con las URLs
         download_urls = {
             "original_video_url": original_video_url,
             "new_video_url": new_video_url
         }
-                    
+
         return jsonify(download_urls), 200
-    
+
+    @blp.doc(parameters=[{'name': 'id_task', 'in': 'path', 'description': 'ID de la tarea', 'required': True}])
+    @blp.response(204, TaskSchema(), description="Detalle de la tarea para poder obtener los links de descargar",
+                  example=TaskIdExample[0])
+    @blp.response(404, description="Tarea no encontrada", example=TaskNotFoundExample)
+    @blp.response(422, description="La conversión no ha sido procesada", example=TasKUnprocessedExample)
+    @jwt_required()
+    def delete(self, id_task):
+        """
+        Eliminar una tarea
+        Eliminar una tarea siempre y cuando ya haya sido procesada
+        @param id_task: ID de la tarea
+        """
+        task = Task.query.filter_by(id=id_task).first()
+        if task is None:
+            abort(404, message="Tarea no encontrada")
+        if task.status != 2:
+            abort(422, message="La conversión no ha sido procesada")
+        db.session.delete(task)
+        db.session.commit()
+        final_message = {
+            "code": 204,
+            "message": "Tarea eliminada exitosamente",
+            "status": "DELETED"
+        }
+        return final_message, 204
