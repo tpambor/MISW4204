a043bde56a636c154e68b586a82dd60e3e6056d6
Tim Pambor <t.pambor@uniandes.edu.co>
2023-11-11 10:56:03 +0100

    Cambiar worker a PubSub

---
+++ converter-worker/worker.py
@@ -0,0 +1,56 @@
+import os
+import time
+import subprocess
+import json
+from concurrent import futures
+from sqlalchemy import update
+from google.cloud import pubsub_v1
+from google.cloud import storage
+import db
+from models import Task, TaskStatus
+
+VIDEO_DIR = os.getenv('VIDEO_DIR')
+BUCKET = os.getenv('BUCKET')
+SUBSCRIPTION_NAME = os.getenv('SUBSCRIPTION')
+
+def conversion_callback(message):
+    req = json.loads(message.data)
+
+    id_video = req['id_video']
+    old_format = req['old_format']
+    new_format = req['new_format']
+
+    print(f"Converting video {id_video} from {old_format} to {new_format}")
+
+    path_old = os.path.join(VIDEO_DIR, f'{id_video}.{old_format}')
+    path_new = os.path.join(VIDEO_DIR, f'{id_video}.{new_format}')
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(BUCKET)
+    blob = bucket.blob(f'{id_video}.{old_format}')
+    blob.download_to_filename(path_old)
+
+    tstart = time.monotonic()
+    subprocess.run(['ffmpeg', '-y', '-nostats', '-i', path_old, '-b:v', '2M', path_new], capture_output=True, check=True)
+    duration = time.monotonic() - tstart
+
+    blob = bucket.blob(f'{id_video}.{new_format}')
+    blob.upload_from_filename(path_new)
+
+    os.remove(path_old)
+    os.remove(path_new)
+
+    with db.session() as session:
+        session.execute(
+            update(Task).where(Task.id == id_video).values(status=TaskStatus.PROCESSED)
+        )
+        session.commit()
+
+    print(f"Finished converting video {id_video} in {duration}s")
+
+with pubsub_v1.SubscriberClient() as subscriber:
+    executor = futures.ThreadPoolExecutor(max_workers=1)
+    scheduler = pubsub_v1.subscriber.scheduler.ThreadScheduler(executor)
+    future = subscriber.subscribe(SUBSCRIPTION_NAME, conversion_callback, scheduler=scheduler)
+
+    future.result()
